f4d80f982ac27768702d1a8972c08201e5052fb7
Ofx Steve <stephemutiso19@gmail.com>
2026-08-31 22:12:21 +0300

    Remove temporary database diagnostic

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,6 @@ Route::get('/_diag/login', function () {
             ->header('Content-Type', 'text/plain');
     }
 });
-Route::get('/_diag/db', function () {
-    try {
-        return response()->json(['ok' => (bool) \DB::connection()->getPdo()]);
-    } catch (\Throwable $e) {
-        return response()->json(['ok' => false, 'error' => $e->getMessage()], 500);
-    }
-});
 
 Route::middleware('guest')->group(function () {
     Route::get('/login',  [AuthenticatedSessionController::class, 'create'])->name('login');
