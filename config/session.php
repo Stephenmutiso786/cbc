0b7339d0bdfd74cd8768f9fb1f6d901c91132b7b
Ofx Steve <stephemutiso19@gmail.com>
2026-08-31 22:09:05 +0300

    Fix Render database startup and login sessions

--- config/session.php
+++ config/session.php
@@ -15,7 +15,7 @@ return [
     'cookie'          => env('SESSION_COOKIE', Str::slug(env('APP_NAME', 'laravel'), '_').'_session'),
     'path'            => env('SESSION_PATH', '/'),
     'domain'          => env('SESSION_DOMAIN'),
-    'secure'          => env('SESSION_SECURE_COOKIE'),
+    'secure'          => (bool) env('SESSION_SECURE_COOKIE', false),
     'http_only'       => (bool) env('SESSION_HTTP_ONLY', true),
     'same_site'       => env('SESSION_SAME_SITE', 'lax'),
     'partitioned'     => (bool) env('SESSION_PARTITIONED_COOKIE', false),
