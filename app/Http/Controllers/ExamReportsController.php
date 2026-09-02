8853d63e9d3400d48ff00ee98087d2535a9a0dd5
Ofx Steve <stephemutiso19@gmail.com>
2026-09-02 20:22:54 +0300

    Prevent report timeout during QR rendering

--- app/Http/Controllers/ExamReportsController.php
+++ app/Http/Controllers/ExamReportsController.php
@@ -97,7 +97,10 @@ class ExamReportsController extends Controller
                 'overall_percentage' => round($overallPercentage, 2),
                 'overall_grade' => $this->gradeForPercentage($overallPercentage, $scale),
                 'verificationUrl' => $verificationUrl,
-                'verificationQr' => QrCode::format('svg')->size(82)->margin(1)->generate($verificationUrl),
+                'verificationQrUrl' => URL::signedRoute('reports.verify.qr', [
+                    'exam' => $exam->id,
+                    'learner' => $row['learner']['id'],
+                ]),
             ];
         })->sortBy(fn (array $card) => $card['learner']['name'])->values()->all();
 
