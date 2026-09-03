b0a1e6a91d7e598eac738c4cbf19b2d6b76610a1
Ofx Steve <stephemutiso19@gmail.com>
2026-09-03 15:27:12 +0300

    Avoid duplicate teacher provisioning during deploy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
             DefaultGradingScalesSeeder::class,
             DefaultClassSubjectsSeeder::class,
             AdminUserSeeder::class,
-            ScreenshotTeachersSeeder::class,
         ]);
     }
 }
