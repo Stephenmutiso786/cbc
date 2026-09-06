81e6e2dc47c9487d9226adb437adc414f79c1898
Ofx Steve <stephemutiso19@gmail.com>
2026-09-06 13:02:23 +0300

    Hide verification URL in printed documents

--- resources/views/pdf/verification-footer.blade.php
+++ resources/views/pdf/verification-footer.blade.php
@@ -1,7 +1,7 @@
 @php($verificationUrl = url('/verify'))
 <div class="verification-footer">
     <div class="verification-qr">{!! \SimpleSoftwareIO\QrCode\Facades\QrCode::format('svg')->size(64)->margin(0)->generate($verificationUrl) !!}</div>
-    <div class="verification-copy">Scan to verify this document<br><span>{{ $verificationUrl }}</span></div>
+    <div class="verification-copy">Scan to verify this document</div>
     @if(is_string(config('school.official_stamp_data')) && str_starts_with(config('school.official_stamp_data'), 'data:image/'))
         <img class="verification-stamp" src="{{ config('school.official_stamp_data') }}" alt="Official school stamp">
     @endif
