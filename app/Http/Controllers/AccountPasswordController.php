bd8ef8dc8b7783729dc0700e073d87c5300c402a
Ofx Steve <stephemutiso19@gmail.com>
2026-09-21 22:56:49 +0300

    Fix impersonation redirects and admin password resets

--- app/Http/Controllers/AccountPasswordController.php
+++ app/Http/Controllers/AccountPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Support\PortalRedirector;
 
 class AccountPasswordController extends Controller
 {
@@ -18,6 +19,6 @@ class AccountPasswordController extends Controller
         $data = $request->validate(['password' => ['required', 'string', 'min:8', 'confirmed']]);
         $request->user()->forceFill(['password' => Hash::make($data['password']), 'must_change_password' => false])->save();
 
-        return redirect()->route('student.dashboard')->with('success', 'Password changed successfully.');
+        return redirect()->to(PortalRedirector::destinationFor($request->user()))->with('success', 'Password changed successfully.');
     }
 }
